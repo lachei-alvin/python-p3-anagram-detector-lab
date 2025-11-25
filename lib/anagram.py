e0098439c8c7043454a5bba8a0a93a76a4acf127
alvin koech <alvinkoech2020@gmail.com>
2025-11-25 14:07:33 +0300

    added files

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        return [
+            w
+            for w in words
+            if w.lower() != self.word and sorted(w.lower()) == self.sorted_word
+        ]
